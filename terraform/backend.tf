9a8f3884045180c5118fe2f40f054b7894f0bf36
jayssolutions <137058390+jayssolutions@users.noreply.github.com>
2026-09-10 17:50:30 +0530

    Add S3 backend configuration for Terraform

---
+++ terraform/backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "s3" {
+    bucket         = "devops-e2e-node"
+    key            = "devops-e3e-node.tfstate"
+    region         = "eu-east-1"
+    dynamodb_table = "terraform-locks"
+  }
+}
