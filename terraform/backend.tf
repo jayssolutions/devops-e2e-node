23930a6d47d864397a9ab2f3cb287d30a29401e5
jayssolutions <137058390+jayssolutions@users.noreply.github.com>
2026-09-10 17:58:32 +0530

    Change S3 backend region from eu-east-1 to us-east-1

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "devops-e2e-node"
     key            = "devops-e3e-node.tfstate"
-    region         = "eu-east-1"
+    region         = "us-east-1"
     dynamodb_table = "terraform-locks"
   }
 }
