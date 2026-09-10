8a8770877555b2e115e31759bfe83fbbdc23cbf3
jayssolutions <jayssolutions69@gmail.com>
2026-09-11 01:02:17 +0530

    update s3 bucket details

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -2,8 +2,8 @@ terraform {
   required_version = ">= 1.5.0"
 
   backend "s3" {
-    bucket         = "jays-devops-tf-state-bucket"
-    key            = "devops-e2e-node/terraform.tfstate"
+    bucket         = "jays-07092026-875285643708-us-east-1-an"
+    key            = "node_app/terraform.tfstate"
     region         = "us-east-1"
     use_lockfile = true
     encrypt        = true
