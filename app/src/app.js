881e848164fa2defb95fc383ff6006cfe6d9e07b
jayssolutions <jayssolutions69@gmail.com>
2026-09-10 14:33:54 +0530

    update config details

--- app/src/app.js
+++ app/src/app.js
@@ -27,7 +27,7 @@ app.use((req, res, next) => {
 
 app.get("/", (_req, res) => {
   res.json({
-    service: "devops-assessment-app",
+    service: "devops-e2e-node-app",
     status: "ok",
     hostname: process.env.HOSTNAME || "unknown"
   });
