1b6ddb0de514c7d92de0e25759f877d2a18f4f4c
jayssolutions <137058390+jayssolutions@users.noreply.github.com>
2026-09-10 18:00:09 +0530

    Add use_lockfile option to S3 backend configuration

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -3,6 +3,6 @@ terraform {
     bucket         = "devops-e2e-node"
     key            = "devops-e3e-node.tfstate"
     region         = "us-east-1"
-    dynamodb_table = "terraform-locks"
+    use_lockfile = true 
   }
 }
